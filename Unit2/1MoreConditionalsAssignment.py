7ebba913da6f5f367ad383c0ba55ae0ac7114314
PJ <25prestongoldsmith@usd266.com>
2024-04-10 11:59:20 -0500

    Done with 2.1

--- Unit2/1MoreConditionalsAssignment.py
+++ Unit2/1MoreConditionalsAssignment.py
@@ -1,11 +1,48 @@
 # print("########## 2.1.1 ##########")
-
+word=input("Type in a random word")
+length = len(word)
+print(length)
+print("there are", length, "letters in the word", word)
+print ("Thank you!")
 # print("########## 2.1.2 ##########")
-
+number= float(input("Please type in a number"))
+integer= int(number)
+decimal= number - integer
+print("Integer part:", integer)
+print("Decimal part:", decimal)
 # print("########## 2.1.3 ##########")
-
+age= int(input("How old are you?"))
+if age >= (18):
+    print("You may vote")
+if age <= (18):
+    print("You may not vote.")
 # print("########## 2.1.4 ##########")
-
-# print("########## 2.1.5 ##########")
-
+number1= int(input("Please type in the first number:"))
+number2= int(input("Please type in another number:")) 
+if number1== number2:
+    print("The numbers are equal!")
+elif number1> number2:
+    print("The greater number was:", number1)
+else:
+    print("The greater number was", number2)
+print("########## 2.1.5 ##########")
+name=input("Name:")
+age= int(input("Age:"))
+print("Person 2:")
+name2=input("Name:")
+age2 = int(input("Age:"))
+if age == age2:
+    print (name, "and", name2, "are the same age")
+elif age>age2:
+    print("The elder is", name)
+else:
+    print("The elder is", name2)
 # print("########## 2.1.6 ##########")
+word1= input("Please type in the 1st word:")
+word2= input("Please type in the 2nd word:")
+if word1 == word2:
+    print("You gave the same word twice.")
+elif word2 < word1:
+    print(word2,"comes alphabetically last.")
+else:
+    print(word1," Comes alphabetically last.")
