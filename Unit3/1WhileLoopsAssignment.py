dcce939ecc26e2a2dbf42ef1a7c8665bdaf3853f
PJ <25prestongoldsmith@usd266.com>
2024-05-02 12:50:04 -0500

    Finished 3.2

--- Unit3/1WhileLoopsAssignment.py
+++ Unit3/1WhileLoopsAssignment.py
@@ -1,23 +1,55 @@
 # ========== 3.1.1 ==========
-print("Are you ready?")
-number= int(input("Please type in a number "))
-
-
+number = 2 
+print 
+while True:
+    number=number + 2
+    print(number)
+    if number == 30:
+        break
 # ========== 3.1.2 ==========
-
-# print("Are you ready?")
-# number = int(input("Please type in a number: "))
-# while number = 0:
-# print(number)
-# print("Go!")
+print("Are you ready?")
+number = int(input("Please type in a number: "))
+while True:
+    print(number)
+    number = number - 1
+    if number == 0:
+        break
+print("Go!")
 
 # ========== 3.1.3 ==========
+upperlimit=int(input("Upper limit: "))
+current = 0
+while True:
+    current = current + 1
+    print (current)
+    if current==upperlimit:
+        break
 
 
 # ========== 3.1.4 ==========
-
-
+upperlimit=int(input("Upper limit: "))
+current=1
+while True:
+    print(current)
+    current = current * 2 
+    if current >= upperlimit:
+        break
 # ========== 3.1.5 ==========
-
-
+upperlimit= int(input("Upper limit: "))
+base=int(input("Base"))
+current=1
+while True:
+    print(current)
+    current= current * base 
+    if current >= upperlimit:
+        break
 # ========== 3.1.6 ==========
+limit=int(input("Limit: "))
+total = 0
+current= 0
+while True:
+    current= current + 1
+    total=total+current
+    if total>=limit:
+        break 
+print(total)
--- Unit3/2BasicStringAssignment.py
+++ Unit3/2BasicStringAssignment.py
@@ -1,26 +1,58 @@
 # ========== 3.2.1 ==========
-
-
+string= input("Please type in a string")
+amount= int(input("Please type in an amount: "))
+word= string * amount
+print(word)
 # ========== 3.2.2 ==========
-
-
+string1= input("Please type in string 1: ")
+string2= input("Please type in string 2: ")
+if string1 > string2:
+    print(string1, "is longer")
+else:
+    print(string2, "is longer")
 # ========== 3.2.3 ==========
-
-
+string= input("Please type in a string")
+position= len(string) - 1
+while True:
+    print(string[position])
+    position= position - 1
+    if position== -1:
+        break
 # ========== 3.2.4 ==========
-
-
-# ========== 3.2.5 ==========
-
-
+string=input("Please type in a string: ")
+position= len(string) - 1 
+if string[1] == string[-2]:
+    print("The second and the second to last characters are the same")
+else:
+    print("The second and the last  characters are different.")
+# ========== 3.2.5 ==========.
+width=int(input("Width"))
+letter= ("#")
+print(width* letter)
 # ========== 3.2.6 ==========
-
-
+width=int(input("Width:"))
+letter=("#")
+height= int(input("Height: "))
+num_done= 0
+while True:
+    print (width*letter)
+    num_done= num_done + 1
+    if height == num_done:
+        break
 # ========== 3.2.7 ==========
-
-
+string=input("Please enter string: ")
+print(string)
+print("-"*len(string))
 # ========== 3.2.8 ==========
-
-
+string=input("Please type in a string")
+letter="*"
+stars= letter * (20 - len(string) ) 
+print(stars+ string) 
 # ========== 3.2.9 ==========
-
+word=input("Word: ")
+stars="*"
+print(stars * 30)
+leftspaces= float(30- len(word) -2) // 2
+rightspaces= float(30 - len(word)- 2)// 2
+print("*"+ (leftspaces * " ") + word + (rightspaces * " ") + "*")
+print(stars * 30)
