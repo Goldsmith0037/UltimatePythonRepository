4bb7a7d646705d1ad723958cd82b8be2b9d0e770
PJ <25prestongoldsmith@usd266.com>
2024-05-13 13:00:40 -0500

    movin on

--- Unit3/3SlicesAndSearchingAssignment.py
+++ Unit3/3SlicesAndSearchingAssignment.py
@@ -1,17 +1,57 @@
 # ========== 3.3.1 ==========
-
-
+string=input("Please type in a string:")
+endpos = 1
+while True:
+    print(string[:endpos])
+    endpos=endpos +  1
+    if len(string) + 1  == endpos:
+        break
 # ========== 3.3.2 ==========
-
+string=input("Please type in a string: ")
+startpos=len(string) - 1
+while True:
+    print(string[startpos:len(string)])
+    startpos= startpos - 1
+    if startpos == -1:
+        break
 
 # ========== 3.3.3 ==========
-
+string= input("Please type in a string ")
+if (string.find("a") >= 0):
+    print ("a found ") 
+else:
+    print("a not found")
+if(string.find("e") >= 0):
+    print ("e found")
+else:
+    print("e not found")
+if(string.find("o") >=0):
+    print("o found")
+else:
+    print ("o not found")
 
 # ========== 3.3.4 ==========
-
-
+word=input("please type in a word: ")
+letter = input("Please type in a character")
+foundpos= word.find(letter)
+print(word[foundpos: foundpos + 3])
 # ========== 3.3.5 ==========
-
-
+word=("Please type in a word")
+letter=("Please type in a character")
+foundpos=word.find(letter)
+print(word[foundpos:foundpos + 3])
 # ========== 3.3.6 ==========
+string=input("Please type in a string: ")
+substring=input("Please type in a substring ")
+if string.find(substring) <0:
+    print("The substring does not occur in the string")
+else:
+    remaining= string[string.find(substring) + 1:len(string)]
+    first=string.find(substring)
+    if remaining.FIND(substring) < 0:
+        print("The substring does not occur twice in the string.")
+    else:
+        second=remaining.find(substring)
+        index= first + second + 1
+        print("The second occurence of the substring is at index", index)
 
--- Unit3/4FunctionsIntroAssignment.py
+++ Unit3/4FunctionsIntroAssignment.py
@@ -1,18 +1,51 @@
 # ========== 3.4.1 ==========
+# def seven_dwarfs():
+    # print("Bashful")
+    # print("Doc")
+    # print("Dopey")
+    # print("Grumpy")
+    # print("Happy")
+    # print("Sleepy")
+    # print("Sneezy")
 
-
+# seven_dwarfs()
 # ========== 3.4.2 ==========
+# def first_character(word):
+#     print(word[0])
+
+# first_character('python')
+# first_character('yellow')
+# first_character('tomorrow')
+# first_character('heliotrope')
+# first_character('open')
+# first_character('night')
 
 
 # ========== 3.4.3 ==========
+# def mean(n1, n2, n3):
+#     print((n1+n2+n3) 3)
 
-
+# mean(5, 3, 1)
+# mean(10, 1, 1)
 # ========== 3.4.4 ==========
-
+# def print_many_times(text, times):
+#    counter=0
+#    while True:
+#       counter=counter+1
+#       print (text)
+#       if counter==times:
+#         break
+    
+# print_many_times("All Pythons, except one, grow up", 3)
 
 # ========== 3.4.5 ==========
-
-
+# def hash_square(width):
+#    counter= 0
+#    while True:
+#       counter = counter + 1
+#       print("#" * width )
+#       if counter== width:
+#          break
+      
+# hash_square(5)
 # ========== 3.4.6 ==========
-
-
